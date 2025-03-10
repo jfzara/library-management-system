abafb2a175ba077e7f862f8f317c4210f72e5918
jfzara <zarajeanfabrice@gmail.com>
2025-03-10 06:09:30 -0400

    creation du pom xml

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,5 +1,10 @@
-package controller;
+package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
     
 }
